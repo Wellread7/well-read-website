71248a83adc11ef3a3c3edf58cfa0a7613eb40b0
Wellread7 <asim@well-read.io>
2025-03-27 11:08:10 +0000

    Deploying to gh-pages from @ Wellread7/well-read-website@807ede6d769feca545d39f0105e9758e4a739f61 🚀

---
+++ _next/static/chunks/app/privacy-policy/page-294374a36b5ef7f1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[310,157],{4292:function(e,n,s){Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.bind(s,8722)),Promise.resolve().then(s.bind(s,5518))}},function(e){e.O(0,[792,463,971,69,744],function(){return e(e.s=4292)}),_N_E=e.O()}]);
